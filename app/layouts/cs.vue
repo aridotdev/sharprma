11f092c2b87a91bf4a1577d671584c1a9288188f
arisafaribinridwan <arisafari.ridwan@gmail.com>
2026-01-12 06:29:11 +0700

    feat: add layouts CS

---
+++ app/layouts/cs.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import type { DropdownMenuItem, NavigationMenuItem } from '@nuxt/ui'
+
+const items = computed<NavigationMenuItem[]>(() => [{
+  label: 'Reports'
+}])
+
+const dropdownItems = computed<DropdownMenuItem[][]>(() => [
+  [
+    {
+      label: 'Profile',
+      icon: 'i-lucide-user',
+      onSelect: () => {
+        navigateTo('/cs/profile')
+      }
+    }
+  ],
+  [
+    {
+      label: 'Logout',
+      icon: 'i-lucide-log-out',
+      onSelect: () => {
+        navigateTo('/cs/logout')
+      }
+    }
+  ]
+])
+</script>
+
+<template>
+  <UApp>
+    <UHeader>
+      <template #title>
+        <img
+          src="/Logo.png"
+          class="h-10 w-auto"
+          alt="RMA Portal Logo"
+        >
+      </template>
+
+      <template #right>
+        <div class="flex items-center gap-1.5 lg:flex-none">
+          <UNavigationMenu :items="items" />
+
+          <UColorModeButton />
+
+          <UDropdownMenu :items="dropdownItems">
+            <UButton
+              icon="i-lucide-user"
+              color="neutral"
+              variant="ghost"
+              size="sm"
+              square
+            />
+          </UDropdownMenu>
+        </div>
+      </template>
+
+      <!-- <template #body>
+        <div class="text-right">
+          <UNavigationMenu
+            :items="items"
+            orientation="vertical"
+            class="-mx-2.5"
+          />
+        </div>
+      </template> -->
+    </UHeader>
+
+    <UMain>
+      <slot />
+    </UMain>
+
+    <UFooter />
+  </UApp>
+</template>
